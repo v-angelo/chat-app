b91f82332229c7b1ae678f0e32b230ca1eb5f63f
v-angelo <v.angelo003@gmail.com>
2026-04-27 11:34:22 +0530

    initial commit

---
+++ public/app.js
@@ -0,0 +1,62 @@
+const socket = io("ws://localhost:3500");
+
+const msgInput = document.querySelector("#message");
+const nameInput = document.querySelector("#name");
+const chatRoom = document.querySelector("#room");
+
+const activity = document.querySelector(".activity");
+const usersList = document.querySelector(".user-list");
+const roomList = document.querySelector(".room-list");
+const chatDisplay = document.querySelector(".chat-display");
+
+function sendMessage(e) {
+  e.preventDefault();
+
+  if (nameInput.value && msgInput.value && chatRoom.value) {
+    socket.emit("message", {
+      name: nameInput.value,
+      text: msgInput.value,
+    });
+    msgInput.value = "";
+  }
+  msgInput.focus();
+}
+
+function enterRoom(e) {
+  e.preventDefault();
+
+  if (nameInput.value && chatRoom.value) {
+    socket.emit("enterRoom", {
+      name: nameInput.value,
+      room: chatRoom.value,
+    });
+  }
+}
+
+document.querySelector(".form-msg").addEventListener("submit", sendMessage);
+
+document.querySelector(".form-join").addEventListener("submit", enterRoom);
+
+msgInput.addEventListener("keypress", () => {
+  socket.emit("activity", nameInput.value);
+});
+
+// listen for message events from the server
+socket.on("message", (data) => {
+  activity.textContent = "";
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
+
+let activityTimer;
+
+// listen for activity event
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 2000);
+});
